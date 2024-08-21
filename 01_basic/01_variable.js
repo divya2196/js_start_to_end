316ce4d9c74f1f71db0a2a2484c7831fa8d59045
divyagorule <71879812+divya2196@users.noreply.github.com>
2024-08-21 15:03:43 +0530

    Update 01_variable.js

--- 01_basic/01_variable.js
+++ 01_basic/01_variable.js
@@ -4,3 +4,11 @@ var accountEmail="divya@gmail.com"
 accoutPassword=258369
  console.log(accountId);
  console.table([accountId, accountEmail, accountName, accoutPassword]);
+/*
+Prefer not to use var
+becouse of issue in block scope and function scope
+only use const and let
+
+use console.table([var1, var2]);
+to display multiple variable
+*/
