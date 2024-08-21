2ad6a5748efacc37723df6e6cf62722d6fe65f0d
divyagorule <71879812+divya2196@users.noreply.github.com>
2024-08-21 08:07:49 +0000

    var done

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,6 @@
+const accountId= 12345
+let accountName="divya"
+var accountEmail="divya@gmail.com"
+accoutPassword=258369
+ console.log(accountId);
+ console.table([accountId, accountEmail, accountName, accoutPassword]);
